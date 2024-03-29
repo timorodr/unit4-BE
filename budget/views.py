dba66ed8f7675b384babd7601582b530fb2a1479
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-03-28 19:22:29 -0500

    all routes working

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Transaction
+from rest_framework import viewsets, permissions
+from .serializers import TransactionSerializer
 
-# Create your views here.
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset=Transaction.objects.all()
+    serializer_class=TransactionSerializer
+    permission_classes=[permissions.AllowAny]
